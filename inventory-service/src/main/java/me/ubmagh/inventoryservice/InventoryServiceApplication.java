76092072ca687cae51853dfe5425b4c2abce0e27
Ayoub👾🦆 <ubmagh@gmail.com>
2022-11-06 21:44:56 +0100

    � crÃeated project files

---
+++ inventory-service/src/main/java/me/ubmagh/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package me.ubmagh.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+
+}
